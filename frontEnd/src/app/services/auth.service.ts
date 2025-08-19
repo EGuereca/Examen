6b3dbd466d4192b6127c93cae9cad4856f02f71f
EGuereca <jeguerecamunoz@gmail.com>
2025-08-19 14:33:40 -0600

    Avances leves y le meti tailwind

--- frontEnd/src/app/services/auth.service.ts
+++ frontEnd/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { 
   User, 
   LoginRequest, 
@@ -10,10 +10,58 @@ import {
   MeResponse, 
   LogoutResponse 
 } from '../models/user.model';
+import { environment } from '../environments/enviroment';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  #http = inject(HttpClient);
+  #router = inject(Router);
+  private _user$ = new BehaviorSubject<MeResponse['data'] | null>(null);
+  public userChanges$ = this._user$.asObservable();
 
+  fetchMe(): Observable<MeResponse> {
+    return this.#http.get<MeResponse>(
+      `${environment.apiBaseUrl}/me`
+    ).pipe(
+      tap(res => this._user$.next(res.data))
+    );
+  }
+
+  login(data: LoginRequest): Observable<LoginResponse> {
+    return this.#http.post<LoginResponse>(
+      `${environment.apiBaseUrl}/login`, data, { withCredentials: true }
+    );
+  }
+
+  register(data: RegisterRequest): Observable<RegisterResponse> {
+    return this.#http.post<RegisterResponse>(
+      `${environment.apiBaseUrl}/register`, data, { withCredentials: true }
+    ).pipe(
+      tap(() => {
+        this.fetchMe().subscribe();
+      })
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.#http
+      .post(`${environment.apiBaseUrl}/logout`, {}, { withCredentials: true })
+      .pipe(
+        tap(() => {
+          this._user$.next(null);
+          localStorage.removeItem('access_token');
+          this.#router.navigate(['/login'], { replaceUrl: true });
+        })
+      );
+  }
+
+  forceLogout() {
+    localStorage.removeItem('access_token');
+    this._user$.next(null);
+    this.#router.navigate(['/login'], { replaceUrl: true });
+  }
 }
--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './app/interceptors/token.interceptor';
+import { authErrorInterceptor } from './app/interceptors/auth-error.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    provideHttpClient(withInterceptors([tokenInterceptor, authErrorInterceptor])),
+  ],
+}).catch((err) => console.error(err));
